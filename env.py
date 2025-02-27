58497fd7a27c377f108ce1ff80a84bd1aa4cadfb
fatima <abaijif@gmail.com>
2025-02-27 09:28:32 +0000

    Update SECRET_KEY and settings

--- env.py
+++ env.py
@@ -1,5 +1,7 @@
-import os 
+import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_DLrdPg60olhJ@ep-orange-morning-a23kcmes.eu-central-1.aws.neon.tech/plank_visa_stank_854298"
 )
+os.environ.setdefault("SECRET_KEY", "your_new_secret_key_here")
+
