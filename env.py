f04b74cd01fe3c29be7514ea8c3e39fc41a88f0e
fatima <abaijif@gmail.com>
2025-02-27 20:22:27 +0000

    update Post model fields and add new database URL; adjust requirements for compatibility

--- env.py
+++ env.py
@@ -1,7 +1,12 @@
 import os
 
-os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_DLrdPg60olhJ@ep-orange-morning-a23kcmes.eu-central-1.aws.neon.tech/plank_visa_stank_854298"
-)
+#os.environ.setdefault(
+#    "DATABASE_URL", "postgresql://neondb_owner:npg_DLrdPg60olhJ@ep-orange-morning-a23kcmes.eu-central-1.aws.neon.tech/plank_visa_stank_854298"
+#)
 os.environ.setdefault("SECRET_KEY", "V8jLfg_TOVbQHjwmm8xH82dy9GnndJfTQX7KXLav_IKTGm0DqiH7dG1DSt-teb4fZGE")
 
+# new database url
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_DLrdPg60olhJ@ep-orange-morning-a23kcmes.eu-central-1.aws.neon.tech/perm_chase_stove_663827"
+)
